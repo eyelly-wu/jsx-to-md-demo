230eb178edf58fe84a1f3f5dd212620505d09ea0
Eyelly Wu <eyelly.wu@gmail.com>
2022-12-05 12:25:18 +0800

    docs: update

--- README.md
+++ README.md
@@ -1,30 +1,32 @@
+The current document is generated based on [jsx-to-md](https://github.com/eyelly-wu/jsx-to-md) and [i18n-pro](https://github.com/eyelly-wu/i18n-pro). You can update the current document in real time by editing  `docs/index.tsx` 
+# Heading
 
-# title
+# Heading 1
 
-# First level title
+## Heading 2
 
-## Secondary title
+### Heading 3
 
-### Third level title
+#### Heading 4
 
-#### Fourth level title
+##### Heading 5
 
-##### Fifth level title
+###### Heading 6
 
-###### Sixth level title
+# Font
+*Italic*
 
-# typeface
-*italic*
-
-**bold**
+**Bold**
 
 ***Oblique Bold***
+
+~~Strikethrough~~<br />
 # Reference Block
 
 ## General reference
 
 >text
-## Nested References
+## Nested Reference
 
 >text1
 >>text2
@@ -46,69 +48,131 @@ console.log(text)
 
 [jsx-to-md](https://github.com/eyelly-wu/jsx-to-md)
 # Image
-![caption](abd 'Picture Title')
+![caption](https://commonmark.org/help/images/favicon.png 'Picture Title')
 # list
 
-## Unordered list
+## Semantic type
+
+### Unordered list
+
 * List Item 1
 * List Item 2
 * List Item 3
 
-## Ordered list
+
+### Ordered list
+
 1. List Item 1
 2. List Item 2
 3. List Item 3
 
+
 ### Nested list 
+
 * development
-
    1. front-end
-
       * JavaScript
       * HTML
       * CSS
       * ...
-
    2. back-end
-
       * Java
       * SQL
       * ...
-
-
 * test
-
-   * unit testing 
-
+   * unit testing
       1. JUnit
       2. Jest
       3. Vitest
       4. ...
-
    * E2E test
-
       1. Cypress
       2. ...
-
-
 * ...
 
-# 表格
+
+## Simple type
+
+### Unordered list
+
+* List Item 1
+* List Item 2
+* List Item 3
+
+
+### Ordered list
+
+1. List Item 1
+2. List Item 2
+3. List Item 3
+
+
+### Nested list 
+
+1. development
+   1. front-end
+      * JavaScript
+      * HTML
+      * CSS
+      * ...
+   2. back-end
+      * Java
+      * SQL
+      * ...
+2. test
+   * unit testing
+      1. JUnit
+      2. Jest
+      3. Vitest
+   * E2E test
+      1. Cypress
+      2. ...
+3. ...
+
+
+# Table
 
 |Left Heading|Centered Title|Right Heading|
 |:-|:-:|-:|
-|Random number:9971176973387248|Random number:21624610838078384|Random number:147378555328036|
-|Random number:7281701157483862|Random number:28062210605518323|Random number:7575161737975713|
-|Random number:3594871583579171|Random number:8170678972981|Random number:009395785301425708|
-|Random number:3734298408197618|Random number:6055528852074827|Random number:19999217599677666|
-|Random number:9355554914653295|Random number:995223776649576|Random number:7518679877904328|
-|Random number:5008891516023222|Random number:5936920455355721|Random number:44931118411559967|
-|Random number:2447320047586934|Random number:42291094450602795|Random number:17909365856775405|
-|Random number:5292774172929309|Random number:7955302451739978|Random number:5116034513943115|
-|Random number:7190575531197694|Random number:3368698865257469|Random number:9812675812810316|
-|Random number:902993117523869|Random number:7996693629138119|Random number:7011651512624864|
+|Random number:539954907301808|Random number:3608084788643968|Random number:316614810105065|
+|Random number:8956901915875026|Random number:3413038119209446|Random number:6440420256912285|
+|Random number:7009582821959164|Random number:6771582043580091|Random number:5790581154089256|
+|Random number:1439559598171256|Random number:9372740234648795|Random number:6343381895147073|
+|Random number:3952532004290876|Random number:3365624627990236|Random number:80523085461055|
+|Random number:011825037679655592|Random number:18834602696457825|Random number:629150935509802|
+|Random number:41609608923765196|Random number:5160237921209869|Random number:10113024315202712|
+|Random number:6997242192026634|Random number:07396849625598301|Random number:22432000592358015|
+|Random number:03086282889880332|Random number:049326681610369016|Random number:3537981891214288|
+|Random number:28162707303037804|Random number:9228806490061472|Random number:10349103722433495|
+
+# Task List
+
+## Basics
+
+- [x] done1
+- [ ] undo
+- [x] done2
+
+
+## nesting
+
+- [ ] twoLevel
+  - [ ] child1
+  - [x] child2
+- [ ] threeLevel
+  - [ ] child1
+    - [x] grandchild1
+    - [ ] grandchild2
+  - [ ] child2
+    - [ ] grandchild1
+    - [ ] grandchild2
+  - [x] child3
+    - [x] grandchild1
+    - [x] grandchild2
+- [x] twoLevelEmpty
+
 
 <h1>Native HTML</h1>
 <div style="background: yellow;height: 80px;display: flex;justify-content: center;align-items: center;">
-  <p>Here you can edit HTML by task</p>
+  <p>You can edit HTML here</p>
 </div>
--- docs/index.tsx
+++ docs/index.tsx
@@ -1,7 +1,7 @@
-import React,{
-  BlockQuote, 
-  Bold, 
-  BoldItalic, 
+import React, {
+  BlockQuote,
+  Bold,
+  BoldItalic,
   Break,
   CodeBlock,
   H1,
@@ -10,184 +10,251 @@ import React,{
   H4,
   H5,
   H6,
-  InlineCode, 
-  Italic, 
+  InlineCode,
+  Italic,
+  StrikeThrough,
   Link,
   Image,
   UnorderList,
   ListItem,
   OderList,
   Table,
+  TaskList,
+  List,
+  render,
 } from 'jsx-to-md'
 import { setI18N, i18n } from 'i18n-pro'
 import en from '../i18n/en.json'
 
-export default function MarkDown(props:any){
-
-  const {
-    locale,
-  } = props
+export default function MarkDown(props: any) {
+  const { locale } = props
 
   setI18N({
     locale,
-    langs:{
+    langs: {
       en,
-    }
+    },
   })
 
   return (
     <>
-    <H1>{i18n('标题')}</H1>
-    <H1>{i18n('一级标题')}</H1>
-    <H2>{i18n('二级标题')}</H2>
-    <H3>{i18n('三级标题')}</H3>
-    <H4>{i18n('四级标题')}</H4>
-    <H5>{i18n('五级标题')}</H5>
-    <H6>{i18n('六级标题')}</H6>
+      {i18n(
+        '当前文档基于 {0} 和 {1} 生成，你可以通过编辑{2}来实时更新当前文档',
+        render(
+          <Link href="https://github.com/eyelly-wu/jsx-to-md">jsx-to-md</Link>,
+        ),
+        render(
+          <Link href="https://github.com/eyelly-wu/i18n-pro">i18n-pro</Link>,
+        ),
+        ' `docs/index.tsx` ',
+      )}
+      <H1>{i18n('标题')}</H1>
+      <H1>{i18n('一级标题')}</H1>
+      <H2>{i18n('二级标题')}</H2>
+      <H3>{i18n('三级标题')}</H3>
+      <H4>{i18n('四级标题')}</H4>
+      <H5>{i18n('五级标题')}</H5>
+      <H6>{i18n('六级标题')}</H6>
 
+      <H1>{i18n('字体')}</H1>
+      <Italic>{i18n('斜体')}</Italic>
+      <Break />
+      <Break />
+      <Bold>{i18n('粗体')}</Bold>
+      <Break />
+      <Break />
+      <BoldItalic>{i18n('倾斜加粗')}</BoldItalic>
+      <Break />
+      <Break />
+      <StrikeThrough>{i18n('删除线')}</StrikeThrough>
+      <br />
 
-    <H1>{i18n('字体')}</H1>
-    <Italic>{i18n('斜体')}</Italic>
-    <Break/>
-    <Break/>
-    <Bold>{i18n('粗体')}</Bold>
-    <Break/>
-    <Break/>
-    <BoldItalic>{i18n('倾斜加粗')}</BoldItalic>
-
-
-    <H1>{i18n('引用块')}</H1>
-    <H2>{i18n('普通引用')}</H2>
-    <BlockQuote>
-      text
-    </BlockQuote>
-    <H2>{i18n('嵌套引用')}</H2>
-    <BlockQuote>
-      text1
-      <BlockQuote level={2}>
-        text2
-        <BlockQuote level={3}>
-        text3 
-        <BlockQuote level={4}>
-        text4 
+      <H1>{i18n('引用块')}</H1>
+      <H2>{i18n('普通引用')}</H2>
+      <BlockQuote>text</BlockQuote>
+      <H2>{i18n('嵌套引用')}</H2>
+      <BlockQuote>
+        text1
+        <BlockQuote level={2}>
+          text2
+          <BlockQuote level={3}>
+            text3
+            <BlockQuote level={4}>text4</BlockQuote>
+          </BlockQuote>
+        </BlockQuote>
       </BlockQuote>
-      </BlockQuote>
-      </BlockQuote>
-    </BlockQuote>
 
-
-    <H1>{i18n('代码块')}</H1>
-    <H2>{i18n('单行代码')}</H2>
-    <InlineCode>const text = 'Hello World'</InlineCode>
-    <H2>{i18n('多行代码')}</H2>
-    <CodeBlock langType='js'>
-      {`const text = 'Hello World'
+      <H1>{i18n('代码块')}</H1>
+      <H2>{i18n('单行代码')}</H2>
+      <InlineCode>const text = 'Hello World'</InlineCode>
+      <H2>{i18n('多行代码')}</H2>
+      <CodeBlock
+        langType="js"
+        code={`const text = 'Hello World'
 console.log(text)`}
-    </CodeBlock>
+      />
 
+      <H1>{i18n('超链接')}</H1>
+      <Link href="https://github.com/eyelly-wu/jsx-to-md-demo">
+        jsx-to-md-demo
+      </Link>
+      <Break />
+      <Break />
+      <Link href="https://github.com/eyelly-wu/jsx-to-md">jsx-to-md</Link>
 
-    <H1>{i18n('超链接')}</H1>
-    <Link href="https://github.com/eyelly-wu/jsx-to-md-demo">jsx-to-md-demo</Link>
-    <Break/>
-    <Break/>
-    <Link href="https://github.com/eyelly-wu/jsx-to-md">jsx-to-md</Link>
+      <H1>{i18n('图片')}</H1>
+      <Image
+        title={i18n('图片标题')}
+        alt={i18n('图片说明')}
+        src="https://commonmark.org/help/images/favicon.png"
+      />
 
-    
-    <H1>{i18n('图片')}</H1>
-    <Image title={i18n('图片标题')} alt={i18n('图片说明')} src="abd"/>
-
-
-    <H1>{i18n('列表')}</H1>
-    <H2>{i18n('无序列表')}</H2>
-    <UnorderList>
-      <ListItem>{i18n('列表项1')}</ListItem>
-      <ListItem>{i18n('列表项2')}</ListItem>
-      <ListItem>{i18n('列表项3')}</ListItem>
-    </UnorderList>
-    <H2>{i18n('有序列表')}</H2>
-    <OderList>
-      <ListItem>{i18n('列表项1')}</ListItem>
-      <ListItem>{i18n('列表项2')}</ListItem>
-      <ListItem>{i18n('列表项3')}</ListItem>
-    </OderList>
-    <H3>{i18n('嵌套列表')}</H3>
-    <UnorderList>
-      <ListItem>
-        {i18n('开发')}
-        <OderList level={2}>
-          <ListItem>
-            {i18n('前端')}
-            <UnorderList level={3}>
-              <ListItem>JavaScript</ListItem>
-              <ListItem>HTML</ListItem>
-              <ListItem>CSS</ListItem>
-              <ListItem>...</ListItem>
-            </UnorderList>
-          </ListItem>
-          <ListItem>
-            {i18n('后端')}
-            <UnorderList level={3}>
-              <ListItem>Java</ListItem>
-              <ListItem>SQL</ListItem>
-              <ListItem>...</ListItem>
-            </UnorderList>
-          </ListItem>
-        </OderList>
-      </ListItem>
-      <ListItem>
-        {i18n('测试')}
-        <UnorderList level={2}>
-          <ListItem>
-            {i18n('单元测试')}
-           <OderList level={3}>
-             <ListItem>JUnit</ListItem>
-             <ListItem>Jest</ListItem>
-             <ListItem>Vitest</ListItem>
-             <ListItem>...</ListItem>
-           </OderList>
-          </ListItem>
-          <ListItem>
-            {i18n('E2E测试')}
-            <OderList level={3}>
-              <ListItem>Cypress</ListItem>
-              <ListItem>...</ListItem>
-            </OderList>
-          </ListItem>
-        </UnorderList>
+      <H1>{i18n('列表')}</H1>
+      <H2>{i18n('语义类型')}</H2>
+      <H3>{i18n('无序列表')}</H3>
+      <UnorderList>
+        <ListItem>{i18n('列表项1')}</ListItem>
+        <ListItem>{i18n('列表项2')}</ListItem>
+        <ListItem>{i18n('列表项3')}</ListItem>
+      </UnorderList>
+      <H3>{i18n('有序列表')}</H3>
+      <OderList>
+        <ListItem>{i18n('列表项1')}</ListItem>
+        <ListItem>{i18n('列表项2')}</ListItem>
+        <ListItem>{i18n('列表项3')}</ListItem>
+      </OderList>
+      <H3>{i18n('嵌套列表')}</H3>
+      <UnorderList>
+        <ListItem>
+          {i18n('开发')}
+          <OderList level={2}>
+            <ListItem>
+              {i18n('前端')}
+              <UnorderList level={3}>
+                <ListItem>JavaScript</ListItem>
+                <ListItem>HTML</ListItem>
+                <ListItem>CSS</ListItem>
+                <ListItem>...</ListItem>
+              </UnorderList>
+            </ListItem>
+            <ListItem>
+              {i18n('后端')}
+              <UnorderList level={3}>
+                <ListItem>Java</ListItem>
+                <ListItem>SQL</ListItem>
+                <ListItem>...</ListItem>
+              </UnorderList>
+            </ListItem>
+          </OderList>
+        </ListItem>
+        <ListItem>
+          {i18n('测试')}
+          <UnorderList level={2}>
+            <ListItem>
+              {i18n('单元测试')}
+              <OderList level={3}>
+                <ListItem>JUnit</ListItem>
+                <ListItem>Jest</ListItem>
+                <ListItem>Vitest</ListItem>
+                <ListItem>...</ListItem>
+              </OderList>
+            </ListItem>
+            <ListItem>
+              {i18n('E2E测试')}
+              <OderList level={3}>
+                <ListItem>Cypress</ListItem>
+                <ListItem>...</ListItem>
+              </OderList>
+            </ListItem>
+          </UnorderList>
         </ListItem>
         <ListItem>...</ListItem>
-    </UnorderList>
+      </UnorderList>
+      <H2>{i18n('简易类型')}</H2>
+      <H3>{i18n('无序列表')}</H3>
+      <List items={['U', i18n('列表项1'), i18n('列表项2'), i18n('列表项3')]} />
+      <H3>{i18n('有序列表')}</H3>
+      <List items={['O', i18n('列表项1'), i18n('列表项2'), i18n('列表项3')]} />
+      <H3>{i18n('嵌套列表')}</H3>
+      <List
+        items={[
+          'O',
+          [
+            i18n('开发'),
+            [
+              'O',
+              [i18n('前端'), ['U', 'JavaScript', 'HTML', 'CSS', '...']],
+              [i18n('后端'), ['U', 'Java', 'SQL', '...']],
+            ],
+          ],
+          [
+            i18n('测试'),
+            [
+              'U',
+              [i18n('单元测试'), ['O', 'JUnit', 'Jest', 'Vitest']],
+              [i18n('E2E测试'), ['O', 'Cypress', '...']],
+            ],
+          ],
+          '...',
+        ]}
+      />
 
+      <H1>{i18n('表格')}</H1>
+      <Table
+        columns={[
+          { fieldName: 'field1', title: i18n('靠左标题') },
+          { fieldName: 'field2', title: i18n('居中标题'), align: 'center' },
+          { fieldName: 'field3', title: i18n('靠右标题'), align: 'right' },
+        ]}
+        data={Array(10)
+          .fill(0)
+          .map((item) => {
+            return ['field1', 'field2', 'field3'].reduce((res, item) => {
+              res[item] = i18n('随机数：') + Math.random().toString(10).slice(2)
+              return res
+            }, {})
+          })}
+      />
 
-    <H1>表格</H1>
-    <Table
-      columns={[
-        {fieldName:'field1', title:i18n('靠左标题')},
-        {fieldName:'field2', title:i18n('居中标题'),align:'center'},
-        {fieldName:'field3', title:i18n('靠右标题'),align:'right'},
-      ]}
-      data={
-        Array(10).fill(0).map(item=>{
-          return ['field1','field2','field3'].reduce((res,item)=>{
-            res[item] =i18n('随机数：')+ Math.random().toString(10).slice(2)
-            return res
-          },{})
-        })}
-    />
+      <H1>{i18n('任务列表')}</H1>
+      <H2>{i18n('基础')}</H2>
+      <TaskList items={['done1', ['undo', 0], ['done2', 1]]} />
+      <H2>{i18n('嵌套')}</H2>
+      <TaskList
+        items={[
+          ['twoLevel', [['child1', 0], 'child2']],
+          [
+            'threeLevel',
+            [
+              ['child1', ['grandchild1', ['grandchild2', 0]]],
+              [
+                'child2',
+                [
+                  ['grandchild1', 0],
+                  ['grandchild2', 0],
+                ],
+              ],
+              ['child3', ['grandchild1', 'grandchild2']],
+            ],
+          ],
+          ['twoLevelEmpty', []],
+        ]}
+      />
 
-    <Break/>
-    <h1>{i18n('原生HTML')}</h1>
-    <Break/>
-    <div 
-    style={{
-      background:'yellow',
-      height:80,
-      display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-    }}>
-      <p>{i18n('这里可以任务编辑HTML')}</p>
-    </div>
+      <Break />
+      <h1>{i18n('原生HTML')}</h1>
+      <Break />
+      <div
+        style={{
+          background: 'yellow',
+          height: 80,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>{i18n('这里可以任意编辑HTML')}</p>
+      </div>
     </>
   )
 }
